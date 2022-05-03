835707c55d41e872c2a6ce3ccbc18a53d1a13a0c
valentichu <valentichu@foxmail.com>
2022-05-03 17:19:34 +0800

    精简代码

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -4,25 +4,18 @@ import {
   ArgumentsHost,
   HttpException,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { ResultData } from '../vo/result';
-import { CommonLoggerService } from '../log/logger.instance';
 
 @Catch(HttpException)
 export class GlobalExceptionFilter<T extends HttpException>
-  implements ExceptionFilter
-{
-  constructor(private readonly commonLoggerService: CommonLoggerService) {}
-
+  implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const msg = exception.message;
     const result = new ResultData(status, msg);
-    if (status >= 500) {
-      this.commonLoggerService.error(exception);
-    }
     response
       .status(200)
       .header('Content-Type', 'application/json; charset=utf-8')
